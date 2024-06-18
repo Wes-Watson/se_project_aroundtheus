00b44f2217db8ba00900a220c5cb982a3a03b9fa
Wes Watson <wesrwats@gmail.com>
2024-06-18 17:14:05 -0500

    Refactor

---
+++ components/Card.js
@@ -0,0 +1,53 @@
+export default class Card {
+  constructor([{ name, link }], cardSelector) {
+    this._name = name;
+    this._link = link;
+    this._cardSelector = cardSelector;
+    console.log(this._cardSelector);
+    //this._handleImageClick = handleImageClick;
+  }
+
+  _setEventListeners() {
+    this._likeButton = this._element.querySelector(".card__like-button");
+    this._likeButton.addEventListener("click", () => {
+      this._handlelikeButton();
+    });
+
+    //this._cardImage.addEventListener("click", () => {
+    //this._handleImageClick(this);
+    //});
+
+    this._element
+      .querySelector(".card__delete-button")
+      .addEventListener("click", () => {
+        this._handleDeleteButton();
+      });
+  }
+
+  _handlelikeButton() {
+    this._likeButton.classList.toggle("card__like-button_active").style;
+    console.log(this._likeButton);
+  }
+
+  _handleDeleteButton() {
+    this._cardElement.remove();
+  }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".card")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
+  getCard() {
+    this._element = this._getTemplate();
+    this._setEventListeners();
+    this._element.querySelector(".card__image").style.backgroundImage =
+      this._link;
+    this._element.querySelector(".card__text").textContent = this._name;
+    return this._element;
+  }
+}
